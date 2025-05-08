33f4b4bd68a2a9fa3afba2d480d22718b4939d7e
Agatha <agatha.g@aluno.senai.br>
2025-05-08 16:19:06 -0300

    feat: fializando as paginas do projeto Event+

--- src/pages/listagemEventos/ListagemEventos.jsx
+++ src/pages/listagemEventos/ListagemEventos.jsx
@@ -1,63 +1,57 @@
-import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
-import "./ListagemEventos.css";
-import Comentario from "../../assets/img/coment.png";
-import ParticiparSim from "../../assets/img/participar-sim.png";
-import ParticiparNao from "../../assets/img/participar-nao.png";
+import Header from "../../components/header/Header";
+import "./ListagemEventos.css"
+import Comentario from "../../assets/img/coment.png"
+import ParticiparSim from "../../assets/img/participar-sim.png"
+import ParticiparNao from "../../assets/img/participar-nao.png"
+import Toggle from "../../components/toggle/Toggle";
+
+
 
 const ListagemEventos = () => {
-    return(
+    return (
         <>
-            <Header
-              visibilBotao="none"  
-              adm="Aluno"
+            <Header nomeUsu="Aluno" 
+                visibilBotao="none"
+                adm="Administrador"
             />
-            <main>
-                <section className="layout_grid listagem_eventos">
-                    <h1>Eventos</h1>
-                    <hr/>
-
-                    <div className="campo_cad_eventos">
-                            <label htmlFor="tipo evento"></label>
-                            <select name="tipo evento" id="">
-                                <option value=""disabled selected>Todos os eventos</option>
-                                <option value="">op 1</option>
-                                <option value="">op 2</option>
-                                <option value="">op 3</option>
-                            </select>
-                        </div>
-
-                <div className="tabela_listagem">
+            <section className="listagem_evento">
+                <h1>Eventos</h1>
+                <hr />
+                <div className="tabela_evento">
+                    <select name="Todos os Eventos" id="" className="select_evento">
+                        <option value="" disabled selected>Todos os Eventos</option>
+                        <option value="">op 1</option>
+                        <option value="">op 2</option>
+                        <option value=""> op 3</option>
+                    </select>
                     <thead>
-                        <tr className="table_cabecalho_eventos">
-                            <th>Título</th>
+                        <tr className="table_evento">
+                            <th>Titulo</th>
                             <th>Tipo Evento</th>
                             <th>Comentários</th>
                             <th>Participar</th>
                         </tr>
                     </thead>
                     <tbody>
-                    <tr className="item_listagem">
-                        <td data-cell="Titulo" >Nome Evento</td>
-                        <td data-cell="Tipo Evento">Titulo Evento</td>
-                        <td data-cell="Comentários"><img src={Comentario} alt="Comentario" /></td>
-                        <td data-cell="Excluir"><img src={ParticiparNao} alt="Lixeira" /></td>
-                    </tr>
-                </tbody>
+                        <tr className="item_evento">
+                            <td data-cell="Nome" >Nome Evento</td>
+                            <td data-cell="Evento">Tipo Evento</td>
+                            <td data-cell="Comentar"><img src={Comentario} alt="Imagem de um comentario" /></td>
+                            <td data-cell="Botao"><Toggle /></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr className="item_evento">
+                            <td data-cell="Nome" >Nome Evento</td>
+                            <td data-cell="Evento">Tipo Evento</td>
+                            <td data-cell="Comentar"><img src={Comentario} alt="Imagem de um comentario" /></td>
+                            <td data-cell="Botao"><Toggle /></td>
+                        </tr>
+                    </tbody>
                 </div>
-
-
-
-
-
-
-
-
-
-                </section>
-
-            </main>
-            <Footer/>
+            </section>
+            <Footer />
         </>
     )
 }
